7107d119c9365f7fdb5c37a2170c7cb54dc5bc22
Wayne <Wayne@Wayne-Laptop>
2018-03-31 12:13:14 -0400

    delete s

--- gsu/src/gsu/dbs/auction/Launcher.java
+++ gsu/src/gsu/dbs/auction/Launcher.java
@@ -120,7 +120,7 @@ public class Launcher extends Application {
 			
 		});
 		
-		// New Vendor Button
+		// New Vendor Buttonn
 		Button newVendorBtn = new Button("Create New Vendor");
 		newVendorBtn.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {    //every time you click button, this is what happens.
