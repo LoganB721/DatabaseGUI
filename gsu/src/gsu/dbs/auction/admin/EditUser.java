e6fc8e4b33c48b8fb064de1b9c246f43415af42d
bbosw <bbosw@10.40.49.204>
2018-04-18 14:24:51 -0400

    Added a custom query box to admin page

--- gsu/src/gsu/dbs/auction/admin/EditUser.java
+++ gsu/src/gsu/dbs/auction/admin/EditUser.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import gsu.dbs.auction.DBConnect;
 import gsu.dbs.auction.Launcher;
 import gsu.dbs.auction.login.BrowsePage;
+import gsu.dbs.auction.newuser.NewUserPage;
 import gsu.dbs.auction.ui.Page;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
@@ -17,8 +18,10 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -45,7 +48,11 @@ public class EditUser extends Page{
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(15);
 		grid.setVgap(10);
-		grid.setPadding(new Insets(25, 25, 25, 25));
+		grid.setPadding(new Insets(50, 50, 50, 50));
+
+		//Vbox for combo box and custom query
+		VBox queries = new VBox(25);
+	
 
 		//Dropdown menu for admin to pick what to edit
 		ObservableList<String> options = 
@@ -58,45 +65,58 @@ public class EditUser extends Page{
 						"Edit Product Type"
 						);
 		final ComboBox<String> comboBox = new ComboBox<String>(options); 
+		final TextArea querysearch = new TextArea();
+		querysearch.setPrefColumnCount(50);
+		querysearch.setPrefRowCount(10);
+		final Button submitquery = new Button("Submit Query");
+	
+		queries.getChildren().addAll(comboBox, querysearch, submitquery);
+		submitquery.setOnAction(new EventHandler<ActionEvent> () {
 
-		grid.add(comboBox, 0, 0);
+			@Override
+			public void handle(ActionEvent event) {
+				buildData(querysearch.getText());
+				grid.add(tv, 1, 0);
+			}
+			
+		});
+		
+		
+		grid.add(queries, 0, 0);
 		//Create Table and columns for users
 
 		comboBox.setOnAction((event)->{
 			String selected = comboBox.getSelectionModel().getSelectedItem();
 
 			if(selected.contains("User")){
-				tv = new TableView();
 				buildData("select * from User");
 				grid.add(tv, 1, 0);
 			}
 			if(selected.contains("Admins")) {
-				tv = new TableView();
 				buildData("select u.*"
 						+ "from User u join Administrator a on u.AccountID = a.AdminID "
 						+ "where u.AccessLevel >= 3 ");
 				grid.add(tv, 1, 0);
 			}
 			if(selected.contains("Customers")){
-				tv = new TableView();
 				buildData("select u.*"
 						+ "from User u join Customer c on u.AccountID = c.CustomerID "
 						+ "where AccessLevel >= 1 ");
 				grid.add(tv, 1, 0);
 			}    
 			if(selected.contains("Vendors")) {				//add in customer review info as well
-				tv = new TableView();
+			
 				buildData("select u.* from User u join Vendor v on u.AccountID = v.VendorID "
 						+ "where AccessLevel >= 2");
 				grid.add(tv, 1, 0);
 			}
 			if(selected.contains("Products")) {
-				tv = new TableView();
+			
 				buildData("select * from Products");
 				grid.add(tv, 1, 0);
 			}
 			if(selected.contains("Edit Product Type")) {
-				tv = new TableView();
+				
 				buildData("select * from Product_Type");
 				grid.add(tv, 1, 0);
 			}
@@ -137,6 +157,8 @@ public class EditUser extends Page{
 		//Back Button
 		Button back = new Button("Back to browse page");
 		Button addUser = new Button("Add User");
+		Hyperlink cantfind = new Hyperlink("Can't find what you're looking for?");
+		
 		HBox hbBack = new HBox();
 		hbBack.setAlignment(Pos.BOTTOM_CENTER);
 		hbBack.getChildren().addAll(back,addUser);
@@ -159,13 +181,15 @@ public class EditUser extends Page{
 
 			}
 		});
-
+		
+		
 		mainPage.getChildren().addAll(grid);
 
 	}
 
 	public void buildData(String query){
 		data = FXCollections.observableArrayList();
+		tv = new TableView();
 		try{
 			c = DBConnect.getConnection();
 			//Query for SQL
