511d95831ae1070e6d740a5bc664b80c54359538
orange451 <drew.hamilton@mac.com>
2018-04-22 14:03:58 -0400

    Started browse page implementation

--- gsu/src/gsu/dbs/auction/login/BrowsePage.java
+++ gsu/src/gsu/dbs/auction/login/BrowsePage.java
@@ -1,5 +1,11 @@
 package gsu.dbs.auction.login;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import gsu.dbs.auction.DBConnect;
 import gsu.dbs.auction.Launcher;
 import gsu.dbs.auction.LoginInformation;
 import gsu.dbs.auction.newuser.NewUserPage;
@@ -44,17 +50,51 @@ public class BrowsePage extends Page {
 		itemPane.setMinWidth(600);
 		mainPage.getChildren().add(itemPane);
 		
+		ListObject[] objects = getItemsOnSale();
+		
 		GridPane itemGrid = new GridPane();
 		itemGrid.setAlignment(Pos.TOP_CENTER);
 		itemGrid.setHgap(10);
 		itemGrid.setVgap(10);
-		for (int i = 1; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				loadItem(itemGrid, i, j, "TestItem" + (i + j*4));
-			}
+		for (int i = 0; i < objects.length; i++) {
+			loadItem(itemGrid, i, 0, objects[i].productName);
 		}
 		
 		itemPane.getChildren().add(itemGrid);
+	}
+
+	private ListObject[] getItemsOnSale() {
+		String SQL = "SELECT bi.*,ProductName,ImageURL FROM Bidding_Items bi JOIN Products p ON bi.BiddingItemID = p.ProductID WHERE bi.SaleStatus = 0;";
+		try {
+			Connection c = DBConnect.getConnection();
+			ResultSet result = c.createStatement().executeQuery(SQL);
+			ArrayList<ListObject> temp = new ArrayList<ListObject>();
+			while(result.next()) {
+				String productName = result.getString(5);
+				String imageURL = result.getString(6);
+				String endTime = result.getString(3);
+				ListObject o = new ListObject(productName, imageURL, endTime);
+				temp.add(o);
+			}
+			return temp.toArray(new ListObject[temp.size()]);
+		} catch (SQLException e) {
+			Launcher.error("Error connecting to database. Please try again later");
+		}
+		
+		return null;
+	}
+	
+	static class ListObject {
+		String productName;
+		String imageURL;
+		String endTime;
+		
+		public ListObject(String productName, String imageURL, String endTime) {
+			this.productName = productName;
+			this.imageURL = imageURL;
+			this.endTime = endTime;
+		}
+		
 	}
 
 	private void loadItem(GridPane grid, int column, int row, String name) {
